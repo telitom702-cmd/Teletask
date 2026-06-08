f3893d270b12f3d66457c0de4572f0d5203f518b
telitom702-cmd <telitom702@gmail.com>
2026-06-08 12:36:30 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,13 @@
-d
+module.exports = {
+    // ডাটাবেস নাম (telitask) সরাসরি লিংকের ভেতরে যোগ করে দেওয়া হয়েছে
+    MONGO_URI: "mongodb+srv://mongodbpy_db_user:pPgtRKyHsm8GvJF2@cluster0.u2ft5ps.mongodb.net/telitask?retryWrites=true&w=majority&appName=Cluster0", 
+    
+    // ⚠️ এখানে আপনার টেলিগ্রাম নাম্বারিক আইডি দিতে হবে (যেমন: 123456789)
+    ADMIN_TELEGRAM_ID: "8248792819", 
+    
+    // ⚠️ BotFather থেকে নেওয়া আপনার বটের টোকেন দিন
+    BOT_TOKEN: "আপনার_বট_টোকেন_এখানে_দিন",
+
+    // পোর্ট অটোমেটিক সিলেক্ট হবে
+    PORT: process.env.PORT || 5000
+};
